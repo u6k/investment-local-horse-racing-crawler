d945b95e2d3c802756a68feadd47aa6c9a7696a6
u6k <u6k.apps@gmail.com>
2020-03-30 17:51:30 +0900

    Added: parse race info, denma

--- investment_local_horse_racing_crawler/spiders/local_horse_racing_spider.py
+++ investment_local_horse_racing_crawler/spiders/local_horse_racing_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+from scrapy.loader import ItemLoader
+
+from investment_local_horse_racing_crawler.items import RaceInfoItem, RaceDenmaItem
 
 
 class LocalHorseRacingSpider(scrapy.Spider):
@@ -52,13 +55,80 @@ class LocalHorseRacingSpider(scrapy.Spider):
         """ Parse race denma page.
 
         @url https://www.oddspark.com/keiba/RaceList.do?sponsorCd=04&raceDy=20200301&opTrackCd=03&raceNb=1
-        @returns items 0 0
+        @returns items 1
         @returns requests 1
         @race_denma
         """
 
         self.logger.info(f"#parse_race_denma: start: url={response.url}")
 
+        # Parse race info
+        self.logger.debug("#parse_race_denma: parse race info")
+
+        loader = ItemLoader(item=RaceInfoItem(), response=response)
+        race_id = response.url.split("?")[-1]
+        loader.add_value("race_id", race_id)
+        loader.add_xpath("race_round", "//div[@id='RCdata1']/span/text()")
+        loader.add_xpath("race_name", "//div[@id='RCdata2']/h3/text()")
+        loader.add_xpath("start_date", "//div[@id='RCdata2']/ul/li[@class='RCdate']/text()")
+        loader.add_xpath("place_name", "//div[@id='RCdata2']/ul/li[@class='RCnum']/text()")
+        loader.add_xpath("course_type_length", "//div[@id='RCdata2']/ul/li[@class='RCdst']/text()")
+        loader.add_xpath("start_time", "//div[@id='RCdata2']/ul/li[@class='RCstm']/text()")
+        loader.add_xpath("weather", "//div[@id='RCdata2']/ul/li[@class='RCwthr']/img/@src")
+        loader.add_xpath("moisture", "//div[@id='RCdata2']/ul/li[@class='RCwatr']/span[@class='baba']/text()")
+        loader.add_xpath("added_money", "//div[@id='RCdata2']/p/text()")
+        i = loader.load_item()
+
+        self.logger.info(f"#parse_race_denma: race info={i}")
+        yield i
+
+        # Parse race denma
+        self.logger.debug("#parse_race_denma: parse race denma")
+
+        for tr in response.xpath("//table[contains(@class,'ent1')]/tr"):
+            if len(tr.xpath("td")) == 0:
+                continue
+
+            if len(tr.xpath("td")) == 15:
+                bracket_number = tr.xpath("td[1]/text()").get()
+
+                loader = ItemLoader(item=RaceDenmaItem(), selector=tr)
+                loader.add_value("race_id", race_id)
+                loader.add_value("bracket_number", bracket_number)
+                loader.add_xpath("horse_number", "td[3]/text()")
+                loader.add_xpath("horse_id", "td[5]/a/@href")
+                loader.add_xpath("jockey_id", "td[6]/a[1]/@href")
+                loader.add_xpath("jockey_weight", "td[6]/strong/text()")
+                loader.add_xpath("trainer_id", "td[6]/a[2]/@href")
+                loader.add_xpath("odds_win", "td[7]/span/text()")
+                loader.add_xpath("favorite", "td[7]/text()")
+                loader.add_xpath("horse_weight", "td[8]/text()[1]")
+                loader.add_xpath("horse_weight_diff", "td[8]/text()[2]")
+                i = loader.load_item()
+
+                self.logger.info(f"#parse_race_denma: race denma={i}")
+                yield i
+            elif len(tr.xpath("td")) == 13:
+                loader = ItemLoader(item=RaceDenmaItem(), selector=tr)
+                loader.add_value("race_id", race_id)
+                loader.add_value("bracket_number", bracket_number)
+                loader.add_xpath("horse_number", "td[1]/text()")
+                loader.add_xpath("horse_id", "td[3]/a/@href")
+                loader.add_xpath("jockey_id", "td[4]/a[1]/@href")
+                loader.add_xpath("jockey_weight", "td[4]/strong/text()")
+                loader.add_xpath("trainer_id", "td[4]/a[2]/@href")
+                loader.add_xpath("odds_win", "td[5]/span/text()")
+                loader.add_xpath("favorite", "td[5]/text()")
+                loader.add_xpath("horse_weight", "td[6]/text()[1]")
+                loader.add_xpath("horse_weight_diff", "td[6]/text()[2]")
+                i = loader.load_item()
+
+                self.logger.info(f"#parse_race_denma: race denma={i}")
+                yield i
+            else:
+                self.logger.warn("#parse_race_denma: unknown record")
+
+        # Parse link
         for a in response.xpath("//a"):
             href = a.xpath("@href").get()
 
