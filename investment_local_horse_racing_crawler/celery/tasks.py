cabc49678e15d18f4fe758eb02c47c4c118e96ef
u6k <u6k.apps@gmail.com>
2020-04-16 00:34:44 +0900

    Added: execute spider on celery

--- investment_local_horse_racing_crawler/celery/tasks.py
+++ investment_local_horse_racing_crawler/celery/tasks.py
@@ -3,6 +3,7 @@ from celery import Celery
 from celery_slack import Slackify
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from billiard import Process
 
 
 app = Celery("tasks")
@@ -19,15 +20,35 @@ if os.getenv("CELERY_SLACK_WEBHOOK"):
     slack_app = Slackify(app, os.getenv("CELERY_SLACK_WEBHOOK"))
 
 
+class CrawlerScript():
+    def __init__(self):
+        settings = get_project_settings()
+        settings_ex = {
+            "APP_RECRAWL_RACE": "sponsorCd=20&raceDy=20200402&opTrackCd=42&raceNb=2"
+        }
+        settings.update(settings_ex)
+
+        self.crawler = CrawlerProcess(settings)
+
+    def _crawl(self):
+        self.crawler.crawl("local_horse_racing")
+        self.crawler.start()
+        self.crawler.stop()
+
+    def crawl(self):
+        process = Process(target=self._crawl)
+        process.start()
+        process.join()
+
+
 @app.task
 def health():
     return "ok"
 
 
+crawler = CrawlerScript()
+
+
 @app.task
 def crawl():
-    settings = get_project_settings()
-
-    process = CrawlerProcess(settings)
-    process.crawl("local_horse_racing")
-    process.start()
+    crawler.crawl()
