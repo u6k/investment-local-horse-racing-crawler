2a99a5d76d1eb2c7a1c3424adcab2ad127af8d14
u6k <u6k.apps@gmail.com>
2022-08-23 01:38:09 +0900

    Added: 確認くんスパイダーを作成した
    プロキシーなどの設定が反映されていることを動作確認するため。

---
+++ investment_local_horse_racing_crawler/spiders/kakunin_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class KakuninSpider(scrapy.Spider):
+    name = "kakunin"
+
+    def __init__(self, *args, **kwargs):
+        super(KakuninSpider, self).__init__(*args, **kwargs)
+
+        self.start_urls = ["https://kakunin.net/kun/"]
+
+    def parse(self, response):
+        pass
