33618026c1cc0bc4ee2a6eb9e41a1a86e4b74369
u6k <u6k.apps@gmail.com>
2023-12-17 02:01:51 +0900

    Added: S3ストレージを使うように設定した

--- local_horse_racing_crawler/settings.py
+++ local_horse_racing_crawler/settings.py
@@ -11,58 +11,16 @@ NEWSPIDER_MODULE = 'local_horse_racing_crawler.spiders'
 
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_IP = 0
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 3
 DOWNLOAD_TIMEOUT = 60
 RETRY_TIMES = 10
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
-COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-TELNETCONSOLE_ENABLED = False
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    'local_horse_racing_crawler.middlewares.InvestmentLocalHorseRacingCrawlerSpiderMiddleware': 543,
-# }
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'local_horse_racing_crawler.middlewares.InvestmentLocalHorseRacingCrawlerDownloaderMiddleware': 543,
-# }
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'local_horse_racing_crawler.pipelines.InvestmentLocalHorseRacingCrawlerPipeline': 300,
-# }
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
-AUTOTHROTTLE_START_DELAY = 5
-AUTOTHROTTLE_MAX_DELAY = 60
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 0
-HTTPCACHE_DIR = 'httpcache'
-HTTPCACHE_IGNORE_HTTP_CODES = []
-HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-HTTPCACHE_GZIP = True
+HTTPCACHE_STORAGE = "local_horse_racing_crawler.middlewares.S3CacheStorage"
 
 SPIDER_CONTRACTS = {
     "local_horse_racing_crawler.contracts.CalendarContract": 10,
@@ -81,6 +39,11 @@ SPIDER_CONTRACTS = {
     "local_horse_racing_crawler.contracts.RaceResultContract": 10,
 }
 
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
+
 logging.getLogger("boto3").setLevel(logging.INFO)
 logging.getLogger("botocore").setLevel(logging.INFO)
 
@@ -92,3 +55,10 @@ AWS_S3_CACHE_FOLDER = os.environ["AWS_S3_CACHE_FOLDER"]
 
 RECACHE_RACE = strtobool(os.environ.get("RECACHE_RACE", "False"))
 RECACHE_DATA = strtobool(os.environ.get("RECACHE_DATA", "False"))
+
+FEEDS = {
+    os.environ["AWS_S3_FEED_URL"]: {
+        "format": "json",
+        "encoding": "utf8",
+    }
+}
